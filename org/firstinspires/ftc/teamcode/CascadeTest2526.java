687bcdb30a57cd24d2a3e6c00eaa56886223a455
Wesley <114669697+pi-this@users.noreply.github.com>
2025-10-14 14:12:34 -0400

    Update CascadeTest2526.java

--- org/firstinspires/ftc/teamcode/CascadeTest2526.java
+++ org/firstinspires/ftc/teamcode/CascadeTest2526.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -30,6 +31,9 @@ public class CascadeTest2526 extends LinearOpMode{
 	@Override
 	public void runOpMode() /*throws InterruptedException*/ {
 		DcMotor BR = hardwareMap.dcMotor.get("BR 0");
+		DcMotor BL = hardwareMap.dcMotor.get("BL 2");
+		DcMotor FR = hardwareMap.dcMotor.get("FR 1");
+		DcMotor FL = hardwareMap.dcMotor.get("FL 3");
 		
 		
 		pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "Pinpoint");
@@ -38,16 +42,67 @@ public class CascadeTest2526 extends LinearOpMode{
 		pinpoint.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
 		
 		//pinpoint.doInitialize();
-		pinpoint.setOffsets(3.75d, 0.5d);
+		pinpoint.setOffsets(95.25d, 12.7d);
 		pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
 		
 		pinpoint.resetPosAndIMU();
 		waitForStart();
 		
-		while(true){
+		double speed = 500;
+		double Am = 1000; // max acceleration, mm/s/s
+		double switchDistance = speed*speed/Am;
+		boolean isSwitched = false;
+		
+		
+		
+		double vkp = 0.002d;// 0.002
+		double vki = 0.00002d;
+	
+		double vISum = 0;
+		double vError = 0;
+		double setpoint = 0d;
+		double vfOut = 0;
+		double targetPosPoint = 4_000d;
+		double positionError = 0;
+		
+		ElapsedTime runTime = new ElapsedTime();
+		runTime.reset();
+		double oldTime = 0;
+		
+		
+		while(opModeIsActive()){
+			positionError = -pinpoint.getPosX() + targetPosPoint;
+			
+			if(isSwitched){
+				setpoint = setpoint - (Am*(runTime.time()-oldTime));
+				if(setpoint <= 0){
+					setpoint = 0;
+				}
+			} else{
+				setpoint = speed;
+				if(positionError < switchDistance){
+					isSwitched = true;// this is bad. only works if going forwards
+				}
+			}
+			vError = -setpoint + pinpoint.getVelX();
+			vISum += vError;
+			vfOut = (vkp * vError) + (vki * vISum);
+			
+			
+			BR.setPower(-vfOut);
+			BL.setPower(vfOut);
+			FR.setPower(-vfOut);
+			FL.setPower(vfOut);
+			oldTime = runTime.time();
+			
 			pinpoint.update();
 			telemetry.addData("x", pinpoint.getPosX());
+			telemetry.addData("ve", vError);
+			telemetry.addData("out", vfOut);
+			telemetry.addData("switched", isSwitched);
+			telemetry.addData("setpoint", setpoint);
 			telemetry.update();
+			
 		}
 		
 		//BR.setPower(1);
